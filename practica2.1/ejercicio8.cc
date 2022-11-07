5ad63651764a862007c5b9d0281705a715ef7014
Pablo Pezo <73887830+PabloPezo@users.noreply.github.com>
2022-11-07 10:13:04 +0100

    Create ejercicio8.cc

---
+++ practica2.1/ejercicio8.cc
@@ -0,0 +1,2 @@
+usuarioso@usuarioso-virtualbox:~$ id
+uid=1000(usuarioso) gid=1001(usuarioso) grupos=1001(usuarioso),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),119(lpadmin),130(vboxusers),998(vboxsf),1000(sambashare)
