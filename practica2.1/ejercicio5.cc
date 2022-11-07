e317b316086f023deac5617a5a27d7846d1031e9
Ismael Barahona <79473853+Isbaraho@users.noreply.github.com>
2022-11-07 10:02:44 +0100

    Update ejercicio5.cc

--- practica2.1/ejercicio5.cc
+++ practica2.1/ejercicio5.cc
@@ -1,12 +1,19 @@
-#include <studio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <sys/utsname.h>
-struct utsname ubameData;
+struct utsname unameData;
 
 
 int main()
 {
     uname(&unameData);
     printf("%s",unameData.sysname);
+    printf("%s",unameData.version);
+    printf("%s",unameData.release);
+    printf("%s",unameData.machine);
+    printf("%s",unameData.nodename);
+    printf("%s",unameData.__domainname);
+    printf("/n");
     return 0;
 }
