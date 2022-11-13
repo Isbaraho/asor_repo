c4543854e89584ef4aeed6654dd54deab088f4b1
Ismael Barahona <79473853+Isbaraho@users.noreply.github.com>
2022-11-13 19:04:02 +0100

    Update ejercicio10.cc

--- practica2.1/ejercicio10.cc
+++ practica2.1/ejercicio10.cc
@@ -10,6 +10,8 @@ int main()
     printf("Usuario: %s", getlogin());
 
     struct passwd *info = getpwuid(1000);
-    printf(info);
+    printf("Nombre de usuario: %d \n",info->pw_name);
+    printf("Directorio home: &d \n",info->pw_dir);
+    printf("Descripcion del usuario: %d \n",info->pw_gecos);
     return 1;
 }
