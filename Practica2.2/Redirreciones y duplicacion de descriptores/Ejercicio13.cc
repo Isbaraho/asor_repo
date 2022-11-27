97f2c6afbd812177622369eac3df7900f5f895af
Ismael Barahona <79473853+Isbaraho@users.noreply.github.com>
2022-11-27 14:09:40 +0100

    Update Ejercicio13.cc

--- Practica2.2/Redirreciones y duplicacion de descriptores/Ejercicio13.cc
+++ Practica2.2/Redirreciones y duplicacion de descriptores/Ejercicio13.cc
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv) {
+
+  int fd = open(argv[1], O_CREAT | O_RDWR, 00777);
+  if (fd == -1) {
+    printf("ERROR.\n");
+    return -1;
+  }
+
+  int fd3=dup2(fd,2);
+  int fd2 = dup2(fd, 1);
+
+  printf("Esto se ha reedirigido a d\n", argv[1]);
+
+  dup2(fd2, fd)
+  dup2(fd3, fd)
+
+  return 1;
+}
 
