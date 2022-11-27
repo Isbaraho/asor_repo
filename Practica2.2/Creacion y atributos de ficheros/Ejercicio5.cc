41df78c2a1aec54d62e8023dc38fb3e05725b628
Ismael Barahona <79473853+Isbaraho@users.noreply.github.com>
2022-11-27 12:09:34 +0100

    Update and rename Ejercicio5 to Ejercicio5.cc

---
+++ Practica2.2/Creacion y atributos de ficheros/Ejercicio5.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+    int df=open(/home/cursoredes/Downloads,O_CREAT,0645);
+    
+    return 1;
+}
