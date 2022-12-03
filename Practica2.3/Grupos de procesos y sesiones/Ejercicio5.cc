7bee031655c2e768acb664d64f34b5b971913bef
Pablo Pezo <73887830+PabloPezo@users.noreply.github.com>
2022-12-03 16:17:55 +0100

    Update Ejercicio5.cc

--- Practica2.3/Grupos de procesos y sesiones/Ejercicio5.cc
+++ Practica2.3/Grupos de procesos y sesiones/Ejercicio5.cc
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sched.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 int main()
  {
+    struct rlimit a;
     int pid=getpid();
     printf("PID: %i \n",pid);
     int ppid=getppid();
       printf("PPID: %i \n",ppid);
+    int pgid=getpgid(pid);
+    printf("PGID: %i \n",pgid);
+      int sid=getsid(pid);
+    printf("PGID: %i \n",sid);
     char *path = malloc(sizeof(char)*(4096 + 1));
     char *rpath = getcwd(path, 4096 + 1);
-    printf("Directorio: %i\n",path);
+
+    getrlimit(RLIMIT_NOFILE, &a);
+    printf("Máximo número %li", a.rlim_max);
+    printf("Directorio: %s\n",path);
     return 0;
  }
-
-    
