f2badf306789a23b48bd9d57d1dd4c011b6cfbc5
Pablo Pezo <73887830+PabloPezo@users.noreply.github.com>
2022-12-03 16:43:17 +0100

    Update and rename Ejercicio7 to Ejercicio7 system.cc

---
+++ Practica2.3/Ejecucion de programas/Ejercicio7 system.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char * * argv)
+{
+
+	if (argc < 2) {
+        printf("Uso: %s comando [argumentos...]. Introducir comillas \n");
+		exit(EXIT_FAILURE);
+	}
+
+	
+	// Ejecutar el comando.
+
+	if (system(argv[1]) == -1) {
+		printf("Error: no se pudo ejecutar el comando\n");
+        perror("system()");
+
+		exit(EXIT_FAILURE);
+	}
+
+    printf("El comando terminó de ejecutarse \n");
+	exit(EXIT_SUCCESS);
+}
