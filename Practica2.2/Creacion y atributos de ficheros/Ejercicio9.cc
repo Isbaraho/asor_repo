ecb34064eb7b00527aae67030e9ad89a1bec15ad
Ismael Barahona <79473853+Isbaraho@users.noreply.github.com>
2022-12-31 12:46:59 +0100

    Update Ejercicio9.cc

--- Practica2.2/Creacion y atributos de ficheros/Ejercicio9.cc
+++ Practica2.2/Creacion y atributos de ficheros/Ejercicio9.cc
@@ -9,8 +9,8 @@
 int main() {
   struct stat buff;
   int fch = open("pruebas",O_RDONLY|O_CREAT,S_IRUSR|S_IWUSR);
-  printf("Major asociado: %li\n", (long) minor(buff.st_dev));
-	printf("Minor asociado: %li\n", (long) major(buff.st_dev));
+  printf("Major asociado: %li\n", (long) major(buff.st_dev));
+	printf("Minor asociado: %li\n", (long) minor(buff.st_dev));
   printf("Numero de inodo: %li\n",buff.st_ino);
   printf("Tipo: %i\n",buff.st_mode);
   time_t t = buff.st_atime;
