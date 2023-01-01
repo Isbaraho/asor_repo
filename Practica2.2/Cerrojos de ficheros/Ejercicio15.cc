941ea23be89798104a7e453b0ec5144373aad60f
Pablo Pezo <73887830+PabloPezo@users.noreply.github.com>
2023-01-01 17:52:33 +0100

    Update Ejercicio15.cc

--- Practica2.2/Cerrojos de ficheros/Ejercicio15.cc
+++ Practica2.2/Cerrojos de ficheros/Ejercicio15.cc
@@ -9,9 +9,19 @@
 #include <stdio.h>
 #include <time.h>
 
-int main()
-{
-    int df=open("/home/usuarioso/Descargas/PR2/ficheros_p2/student-records/hola.txt", O_RDWR);
+int main(int argc, char ** argv) {
+
+
+  if (argc < 2) {
+     printf("ERROR: Se debe especeficar la ruta del archivo en los parámetros del programa.\n");
+     return -1;
+  }
+
+  int fd = open(argv[1], O_CREAT | O_RDWR, 00777);
+  if (fd == -1) {
+    printf("ERROR: No se ha podido abrir/crear el fichero.\n");
+    return -1;
+  }
     struct stat hola;
     fstat(df, &hola);
     int a = hola.st_size;
